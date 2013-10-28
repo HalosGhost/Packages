a1d9a919782c6b0c95b3d20d13f0454b0b273204
HalosGhost <halosghost@archlinux.info>
2013-10-27 22:38:50 -0500

    Update for orphaned files and (hopefully) functional wrapper

--- warcarrier-svn/warcarrier.sh
+++ warcarrier-svn/warcarrier.sh
@@ -3,6 +3,6 @@
 export PERL5LIB="/opt/warcarrier/lib/"
 
 (pushd /opt/warcarrier
-case "$0" in
-   warcarrier,wcclean,wcd) ./"$0";;
+case "$(basename $0)" in
+   warcarrier|wcclean|wcd) ./"$(basename $0)";;
 esac)
